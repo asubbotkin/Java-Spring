d4a97b1e2991f84b317e51aedfc6aedab3aee381
Andrew Subbotkin <50634078+asubbotkin@users.noreply.github.com>
2024-05-29 14:24:22 +0300

    added endpoints

--- src/main/java/com/tekwill/course3/controller/EmployeeController.java
+++ src/main/java/com/tekwill/course3/controller/EmployeeController.java
@@ -1,12 +1,49 @@
 package com.tekwill.course3.controller;
 
 import com.tekwill.course3.dto.Employee;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class EmployeeController {
-    public void test(){
-//        Employee emp = new Employee("ghj", "mnb", 20);
+    List<Employee> employeeList = new ArrayList<>();
+
+    @PostMapping("api/register/employee")
+    public ResponseEntity<String> registerEmployee(@RequestBody @Valid Employee inputEmployee) {
+        inputEmployee.setUuid(UUID.randomUUID());
+        employeeList.add(inputEmployee);
+        return ResponseEntity.ok("Employee was added successfully! ID " + inputEmployee.getUuid());
     }
 
+    @GetMapping("api/empoyees/all")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeList);
+    }
+
+    @GetMapping("api/employee/{uuid}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable UUID uuid) {
+        return ResponseEntity.ok(employeeList.stream()
+                .filter(e -> e.getUuid().equals(uuid))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+    }
+
+    @DeleteMapping("api/employee/{uuid}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable UUID uuid) {
+        Optional<Employee> emp = employeeList.stream()
+                .filter(e -> e.getUuid().equals(uuid))
+                .findFirst();
+        if (emp.isEmpty())
+            return ResponseEntity.notFound().build();
+        employeeList.remove(emp);
+        return ResponseEntity.ok("Employee with ID: " + uuid + "was successully deleted!");
+    }
 }
--- src/main/java/com/tekwill/course3/dto/Employee.java
+++ src/main/java/com/tekwill/course3/dto/Employee.java
@@ -1,12 +1,20 @@
 package com.tekwill.course3.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 public class Employee {
+    private UUID uuid;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String surname;
+    @NotNull
     private int age;
 }
